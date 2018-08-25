7612b5b1465a4e231bd47939bc0b37e4721c0de8
Ricardo Willems <RicardoGWill@gmail.com>
2018-08-25 15:19:17 -0400

    Extra space added.

--- app/src/main/java/com/ricardogwill/startedserviceapp/TheService.java
+++ app/src/main/java/com/ricardogwill/startedserviceapp/TheService.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.widget.Toast;
 
 public class TheService extends Service {
+    
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
