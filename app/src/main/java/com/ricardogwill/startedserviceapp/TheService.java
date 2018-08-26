3856cf12b34e89748f37800fd4b8fa04488332f9
Ricardo Willems <RicardoGWill@gmail.com>
2018-08-25 22:39:56 -0400

    I created an inner class called "TheThread" within "TheService" class.  "TheThread" allows things to occur after the Service has started.  If there were no threads, then I would not be able to type or do anything after the Service started except stop the Service.

--- app/src/main/java/com/ricardogwill/startedserviceapp/TheService.java
+++ app/src/main/java/com/ricardogwill/startedserviceapp/TheService.java
@@ -7,7 +7,32 @@ import android.support.annotation.Nullable;
 import android.widget.Toast;
 
 public class TheService extends Service {
-    
+    // New internal class (TheThread) allows threads (and thus implements Runnable).
+    final class TheThread implements Runnable {
+        // Variable to use in Constructor
+        int serviceID;
+        // Constructor
+        public TheThread(int serviceID) {
+            this.serviceID = serviceID;
+        }
+
+        @Override
+        public void run() {
+            // The "run" method is necessary. In this case, we are filling it with a Service
+            // that will take 20 seconds to complete, but WON'T block other processes
+            // because it is asynchronous and runs in a thread.
+            synchronized (this) {
+                try {
+                    wait(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // Stops the Service after 20 seconds.
+                stopSelf(this.serviceID);
+            }
+        }
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -22,6 +47,8 @@ public class TheService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "Service Started.", Toast.LENGTH_SHORT).show();
+        Thread thread = new Thread(new TheThread(startId));
+        thread.start();
         return START_STICKY;
     }
 
